300387b371b511be19f11f0ecd04f26d6d7662f0
Ramyaparanitharan <ramya.paranitharan@optisolbusiness.com>
2026-06-26 20:41:31 +0530

    custom directive

--- src/inputoutput/child/child.ts
+++ src/inputoutput/child/child.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CapitalizePipe } from '../../capitalize-pipe';
+import { HighlightDirective } from '../../highlight';
 
 @Component({
   selector: 'app-child',
-  imports: [CapitalizePipe],
+  imports: [CapitalizePipe, HighlightDirective],
   templateUrl: './child.html',
   styleUrl: './child.scss',
 })
